3f9c58385c90c38b32ac502c30941bf30558603e
Uygar Uyanıksoy <uygaruyaniksoy@gmail.com>
2018-08-18 05:31:29 +0300

    fixed image reloading bug

--- app/src/main/java/com/uygaruyaniksoy/moviedb/AsyncImageTask.java
+++ app/src/main/java/com/uygaruyaniksoy/moviedb/AsyncImageTask.java
@@ -3,19 +3,25 @@ package com.uygaruyaniksoy.moviedb;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.view.View;
 import android.widget.ImageView;
 
 import java.io.InputStream;
+import java.util.Map;
 
 public class AsyncImageTask extends AsyncTask<String, Void, Bitmap> {
+    private final Map<String, Bitmap> cache;
     ImageView image;
+    private String url;
 
-    public AsyncImageTask(ImageView image) {
+    public AsyncImageTask(ImageView image, Map<String, Bitmap> cache) {
         this.image = image;
+        this.cache = cache;
     }
 
     protected Bitmap doInBackground(String... urls) {
         String url = urls[0];
+        this.url = url;
         Bitmap bitmap = null;
         try {
             InputStream in = new java.net.URL(url).openStream();
@@ -28,5 +34,7 @@ public class AsyncImageTask extends AsyncTask<String, Void, Bitmap> {
 
     protected void onPostExecute(Bitmap result) {
         image.setImageBitmap(result);
+        image.setVisibility(View.VISIBLE);
+        cache.put(url, result);
     }
 }
--- app/src/main/java/com/uygaruyaniksoy/moviedb/actors/ActorsListAdapter.java
+++ app/src/main/java/com/uygaruyaniksoy/moviedb/actors/ActorsListAdapter.java
@@ -1,6 +1,7 @@
 package com.uygaruyaniksoy.moviedb.actors;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -15,11 +16,15 @@ import com.uygaruyaniksoy.moviedb.AsyncImageTask;
 import com.uygaruyaniksoy.moviedb.R;
 import com.uygaruyaniksoy.moviedb.actors.domain.Actor;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ActorsListAdapter extends ArrayAdapter<Actor> {
     private final Context context;
     private List<Actor> actors;
+
+    private Map<String, Bitmap> cache = new HashMap<>();
 
     public ActorsListAdapter(@NonNull Context context, @NonNull List<Actor> actors) {
         super(context, R.layout.row_item, actors);
@@ -47,7 +52,13 @@ public class ActorsListAdapter extends ArrayAdapter<Actor> {
         TextView popularityScore = convertView.findViewById(R.id.actorPopularityScore);
         popularityScore.setText(actor.getPopularity().toString());
         ImageView image = convertView.findViewById(R.id.actorImage);
-        new AsyncImageTask(image).execute(actor.getProfilePath());
+        image.setVisibility(View.INVISIBLE);
+        if (cache.containsKey(actor.getProfilePath())) {
+            image.setImageBitmap(cache.get(actor.getProfilePath()));
+            image.setVisibility(View.VISIBLE);
+        } else {
+            new AsyncImageTask(image, cache).execute(actor.getProfilePath());
+        }
         return convertView;
     }
 }
